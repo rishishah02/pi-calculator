0b629340ebe6ce17c1cecb7879c74672357c01cc
rishishah02 <67508489+rishishah02@users.noreply.github.com>
2022-01-16 00:28:50 -0500

    Speed Optimization

--- pi.cpp
+++ pi.cpp
@@ -4,22 +4,23 @@
 #include <random>
 using namespace std;
 
-double distanceFromOrigin(double x, double y){
-    return sqrt((x*x)+(y*y));
-}
-
 double piCalc(int num_sims){
     int insideCircle = 0;
     double randX;
     double randY;
+    
+    // Random Number Generator using this implementation of a uniform random distribution (runif): https://en.cppreference.com/w/cpp/numeric/random/uniform_real_distribution
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
     std::uniform_real_distribution<double> distribution(0.0,1.0);
+    
+    // Create two random x and y coordinates in first quadrant
+    // Check if distance is at most 1 (within the circle)
+    // Count number of points inside quadrant and divide by total points (multiply by 4 to account for entire circle)
     for(int i = 0; i<num_sims; ++i){
         randX = distribution(generator);
         randY = distribution(generator);
-        //cout << randX << ", " << randY << endl;
-        if(distanceFromOrigin(randX, randY)<=1){
+        if(distanceFromOrigin(randX, randY)<=1){ //Avoided Square Root function in distance formula
             insideCircle++;
         }
     }
@@ -28,7 +29,7 @@ double piCalc(int num_sims){
 
 int main(){
     int sims;
-    cout << "Enter Number of Simulations: ";
+    cout << "Enter Number of Simulations: "; // The larger the number of simulations, the closer the approximation will be to the exact value
     cin >> sims;
     cout << endl;
     double pi = piCalc(sims);
